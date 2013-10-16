90983e433da211c05d53fb8a0d5b5f31a72dbc8e
Julie C. Zachman <zachman@engr.wisc.edu>
2013-10-16 13:32:11 -0500

    Initial placement

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# We don't proceed unless the preceding step succeeded, and
+# we return the success or failure of the sequence.
+#
+set -x
+set -e
+
+DIR=`pwd`
+
+cd hdf5-1.8.11
+./configure --prefix=`pwd`/../install
+make
+make install
+cd ..
+DIR=`pwd`
+
+cd moab-4.6.2
+./configure --enable-optimize --enable-shared --disable-debug --without-netcdf --with-hdf5=`pwd`/../install --prefix=`pwd`/../install
+make
+make install 
+cd ..
+DIR=`pwd`
+
+tar -czf results.tar.gz ./install
+exit $?
