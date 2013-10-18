0307bf528567b246c7652c8bd32945b42e6c93f9
Julie C. Zachman <jczachman@wisc.edu>
2013-10-17 21:49:30 -0500

    First draft: download dagmc alpha_release, compile mainfludag

--- build.sh
+++ build.sh
@@ -24,5 +24,18 @@ make install
 cd ..
 DIR=`pwd`
 
-tar -czf results.tar.gz ./install
+cd FLUKA/flutil
+cp rfluka rfluka.orig
+cd ../..
+cp DAGMC/FluDAG/src/rfluka FLUKA/flutil
+
+DIR=`pwd`
+mkdir -p DAGMC/FluDAG/bld
+cd DAGMC/FluDAG/bld
+cmake ../src -DMOAB_HOME=`pwd`/../../../moab-4.6.2
+make
+cd ../../..
+DIR=`pwd`
+
+tar -czf results.tar.gz ./install ./FLUKA/flutil/rfluka* ./DAGMC/FluDAG/bld
 exit $?
