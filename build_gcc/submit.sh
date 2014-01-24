ebea882db4f3901708387f6a8e2a13cc54b16cb5
Julie C. Zachman <jczachman@wisc.edu>
2014-01-23 20:12:30 -0600

    Initial placement of build dir for gcc

---
+++ build_gcc/submit.sh
@@ -0,0 +1,21 @@
+
+if [ -z $1 ]
+then
+   echo "Error: No run-spec specified"
+   exit 1
+fi
+
+path=`pwd`
+echo "
+method    = scp
+scp_file  = $path/build_gcc482.sh
+recursive = true
+">$path/build_gcc482.sh.scp
+
+echo "
+method    = scp
+scp_file  = $path/gcc-4.8.2.tar.bz2
+recursive = true
+">$path/gcc-4.8.2.tar.bz2.scp
+
+nmi_submit $1
