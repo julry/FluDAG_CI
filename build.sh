594f7b2d5431504fa4f8cca4bf900c605a01c961
Andrew Davis <davisa@engr.wisc.edu>
2014-02-13 10:49:56 -0600

    Update build.sh
    Sanitise variables.

--- build.sh
+++ build.sh
@@ -8,52 +8,58 @@
 set -x
 set -e
 
+# original working directory
 OWD=`pwd`
-DIR=`pwd`
+#DIR=`pwd`
 # Out of source build for hdf5.  
-mkdir `pwd`/bld_hdf5
+mkdir $OWD/bld_hdf5
 cd bld_hdf5
-../hdf5-1.8.11/configure --enable-shared --prefix=`pwd`/../install
+# create install dir
+mkdir $OWD/hdf5
+../hdf5-1.8.11/configure --enable-shared --prefix=$OWD/hdf5
 make
 make install
 # set the shared lib path
-export LD_LIBRARY_PATH=`pwd`/../install/lib
+export LD_LIBRARY_PATH=$OWD/hdf5/lib
 # dont need these, but may prove useful in future
-export PATH=$PATH:`pwd`/../install/bin
+export PATH=$PATH:$OWD/hdf5/bin
 cd ..
-DIR=`pwd`
+# back in $OWD
+# DIR=`pwd`
 
 # Build moab with no CGM 
-mkdir `pwd`/bld_moab
+mkdir $OWD/bld_moab
 cd bld_moab
-../moab-4.6.2/configure --enable-optimize --enable-shared --disable-debug --without-netcdf --with-hdf5=`pwd`/../install --prefix=`pwd`/../install
+# make moab install dir
+mkdir $OWD/moab
+../moab-4.6.2/configure --enable-optimize --enable-shared --disable-debug --without-netcdf --with-hdf5=$OWD/hdf5 --prefix=$OWD/moab
 make
 make install 
 # set the shared lib path
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/../install/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OWD/moab/lib
 # dont need them but may be useful
-export PATH=$PATH:`pwd`/../install/bin
+export PATH=$PATH:$OWD/moab/bin
 cd ..
-DIR=`pwd`
+
 
 # Copy rfluka script that allows for longer filenames
 cd ./FLUKA/flutil
 cp rfluka rfluka.orig
 cd ../..
 cp ./DAGMC/FluDAG/src/rfluka FLUKA/flutil
-DIR=`pwd`
+#DIR=`pwd`
 
 # Do not need to make the libflukahp.a library, but do need the environment vars
 export FLUPRO=$OWD/FLUKA
 export FLUFOR=gfortran
-DIR=`pwd`
+#DIR=`pwd`
 
 # Compile the fludag source and link it to the fludag and dagmc libraries
 mkdir -p ./DAGMC/FluDAG/bld
 cd ./DAGMC/FluDAG/bld
 # cmake ../src -DMOAB_HOME=`pwd`/../../../install
 cmake \
--D MOAB_HOME=$OWD/install \
+-D MOAB_HOME=$OWD/moab \
 ../src 
 make
 
@@ -63,7 +69,7 @@ mkdir `pwd`/lib
 cd lib
 cmake ../gtest-1.7.0
 make
-DIR=`pwd`
+#DIR=`pwd`
 
 # Configure and make the unit tests
 cd $OWD/DAGMC/FluDAG/src/test
@@ -71,7 +77,7 @@ mkdir `pwd`/bld
 cd bld
 cmake \
 -D DAGMC_FLUDAG_SOURCE=$OWD/DAGMC/FluDAG/src/ \
--D MOAB_HOME=$OWD/bld_moab/install   \
+-D MOAB_HOME=$OWD/moab   \
 -D GTEST_HOME=$OWD/DAGMC/gtest \
 ..
 make
@@ -79,5 +85,5 @@ make
 # Wrap up the results for downloading
 cd $OWD
 # tar -czf results.tar.gz ./bld_hdf5 ./bld_moab ./install ./FLUKA/flutil/rfluka* ./DAGMC/FluDAG/bld ./DAGMC/gtest/lib ./DAGMC/FluDAG/src/test
-tar -czf results.tar.gz ./install ./FLUKA/flutil/rfluka* ./DAGMC/FluDAG/bld ./DAGMC/gtest/lib ./DAGMC/FluDAG/src/test
+tar -pczf results.tar.gz moab hdf5 ./FLUKA/flutil/rfluka* ./DAGMC/FluDAG/bld ./DAGMC/gtest/lib ./DAGMC/FluDAG/src/test
 exit $?
