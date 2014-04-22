bd5c1ce3dfe9de83b361d253a2997caf2290fb46
Julie C. Zachman <jczachman@wisc.edu>
2014-04-22 09:29:06 -0500

    Ensure tests link to correct gcc libraries

--- run-test.sh
+++ run-test.sh
@@ -1,5 +1,11 @@
 set -x
 set -e
+
+gccdir="$PWD/gccSL6"
+# Ensure all components build with local gcc
+export LD_LIBRARY_PATH=${gccdir}/lib:${gccdir}/lib64
+export PATH=${gccdir}/bin:$PATH
+
 cd DAGMC/FluDAG/bld/tests
 ./fludag_unit_tests --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/FLUDAG.//g'`
 
