c0bf8e7d00bdc9541520db261d372cc05eb7de1b
Andrew Davis <davisa@engr.wisc.edu>
2014-05-06 18:59:24 -0500

    correct test path to dagsolid tests

--- generate_test_list.sh
+++ generate_test_list.sh
@@ -9,8 +9,8 @@ export PATH=${gccdir}/bin:$PATH
 DAGMC/FluDAG/bld/tests/fludag_unit_tests --gtest_list_tests  | python gen_test_list.py FLUDAG.
 DAGMC/FluDAG/bld/tests/fludag_unit_tests --gtest_list_tests  | python gen_test_list.py FLUDAG. > tasklist.nmi
 
-DAGMC/Geant4/dagsolid/bld/tests/dagsolid_unit_tests --gtest_list_tests  | python gen_test_list.py DAGSOLID.
-DAGMC/Geant4/dagsolid/bld/tests/dagsolid_unit_tests --gtest_list_tests  | python gen_test_list.py DAGSOLID. >> tasklist.nmi
+DAGMC/Geant4/dagsolid/bld/test/dagsolid_unit_tests --gtest_list_tests  | python gen_test_list.py DAGSOLID.
+DAGMC/Geant4/dagsolid/bld/test/dagsolid_unit_tests --gtest_list_tests  | python gen_test_list.py DAGSOLID. >> tasklist.nmi
 
 
 exit $?
