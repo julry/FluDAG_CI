934051ce70371507be314ae0f88fe9fcfa4de8fd
Julie C. Zachman <jczachman@wisc.edu>
2014-09-16 17:18:03 -0500

    More LD_LIBRARY_PATH fixes

--- generate_test_list.sh
+++ generate_test_list.sh
@@ -1,11 +1,13 @@
 set -x
 set -e
 
+OWD=$PWD
 # gccdir="$PWD/gccSL6"
 # source ./general_exports.sh
 # Ensure all components build with local gcc
 # export LD_LIBRARY_PATH=${gccdir}/lib:${gccdir}/lib64
 # export PATH=${gccdir}/bin:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OWD/DAGMC/lib/
 
 DAGMC/tests/fludag_unit_tests --gtest_list_tests  | python gen_test_list.py FLUDAG.
 DAGMC/tests/fludag_unit_tests --gtest_list_tests  | python gen_test_list.py FLUDAG. > tasklist.nmi
