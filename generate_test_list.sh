803f1d689e12f39f327eca7dbe8ed0c0a08c1bbb
Julie C. Zachman <jczachman@wisc.edu>
2014-09-16 16:51:00 -0500

    Remove unnecessary call

--- generate_test_list.sh
+++ generate_test_list.sh
@@ -1,8 +1,8 @@
 set -x
 set -e
 
-gccdir="$PWD/gccSL6"
-source ./general_exports.sh
+# gccdir="$PWD/gccSL6"
+# source ./general_exports.sh
 # Ensure all components build with local gcc
 # export LD_LIBRARY_PATH=${gccdir}/lib:${gccdir}/lib64
 # export PATH=${gccdir}/bin:$PATH
