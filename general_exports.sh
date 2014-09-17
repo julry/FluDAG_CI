b128959501fb72c81f89c3862cda085c4b8eea95
Julie C. Zachman <jczachman@wisc.edu>
2014-09-17 11:44:38 -0500

    Set, don't append DAGMC/lib to LD_LIBRARY_PATH; cleanup

--- general_exports.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# general_exports.sh
-# Ensure all components build with local gcc
-export LD_LIBRARY_PATH=${gccdir}/lib:${gccdir}/lib64
-export PATH=${gccdir}/bin:$PATH
--- run-test.sh
+++ run-test.sh
@@ -3,12 +3,8 @@ set -e
 
 OWD=$PWD
 
-# gccdir="$PWD/gccSL6"
-# source ./general_exports.sh
-# Ensure all components build with local gcc
-# export LD_LIBRARY_PATH=${gccdir}/lib:${gccdir}/lib64
-# export PATH=${gccdir}/bin:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OWD/DAGMC/lib/
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OWD/DAGMC/lib/
+export LD_LIBRARY_PATH=$OWD/DAGMC/lib/
 source $OWD/geant4/bld/geant4make.sh
 
 cd DAGMC/tests
