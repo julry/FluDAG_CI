09f2ccc3ad7f5f12fce77c6d3a0fd183a72b2926
Julie C. Zachman <jczachman@wisc.edu>
2014-10-23 16:42:02 -0500

    Add uwuw_unit_tests

--- generate_test_list.sh
+++ generate_test_list.sh
@@ -1,22 +1,17 @@
 set -x
 set -e
 
-OWD=$PWD
-export LD_LIBRARY_PATH=$OWD/anaconda/lib:$OWD/geant4/lib:$OWD/DAGMC/lib
-export PATH=$OWD/anaconda/bin:$OWD/anaconda/usr/local/bin:$PATH
-# on Debian these dirs will exist and are needed
-LAPACK='lapack-3.5.0'
-F2C='libf2c'
-if [ -d $F2C ] && [ -d $LAPACK ]  ; then
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OWD/$F2C:$OWD/$LAPACK
-fi
-
+source paths.sh
 
 DAGMC/tests/fludag_unit_tests --gtest_list_tests  | python gen_test_list.py FLUDAG.
 DAGMC/tests/fludag_unit_tests --gtest_list_tests  | python gen_test_list.py FLUDAG. > tasklist.nmi
 
 DAGMC/tests/dagsolid_unit_tests --gtest_list_tests  | python gen_test_list.py DAGSOLID.
 DAGMC/tests/dagsolid_unit_tests --gtest_list_tests  | python gen_test_list.py DAGSOLID. >> tasklist.nmi
+
+# Add files uwuw_unit_tests
+DAGMC/tests/uwuw_unit_tests --gtest_list_tests | python gen_test_list.py UWUW.
+DAGMC/tests/uwuw_unit_tests --gtest_list_tests | python gen_test_list.py UWUW. >> tasklist.nmi
 
 exit $?
 
--- run-test.sh
+++ run-test.sh
@@ -1,23 +1,17 @@
+# set -e
 set -x
-set -e
 
 OWD=`pwd`
 
 source $OWD/geant4/bld/geant4make.sh
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OWD/anaconda/lib/:$OWD/geant4/lib/:$OWD/DAGMC/lib
-# Pyne section
-export PATH=$OWD/anaconda/bin:$PATH
-
-# on Debian these dirs will exist and are needed
-LAPACK='lapack-3.5.0'
-F2C='libf2c'
-if [ -d $F2C ] && [ -d $LAPACK ]  ; then
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OWD/$F2C:$OWD/$LAPACK
-fi
+# source paths.sh sets these slightly differently
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OWD/anaconda/lib/:$OWD/geant4/lib/:$OWD/DAGMC/lib
+# export PATH=$OWD/anaconda/bin:$PATH
+source $OWD/paths.sh
 
 cd $OWD/DAGMC/tests
 ./fludag_unit_tests --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/FLUDAG.//g'`
 ./dagsolid_unit_tests --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/DAGSOLID.//g'`
-
+./uwuw_unit_tests --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/UWUW.//g'`
 
 exit $?
