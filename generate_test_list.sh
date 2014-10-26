41c14c50a79b5bb3bb1e877afaa56a0f53305b51
Julie C. Zachman <jczachman@wisc.edu>
2014-10-26 11:05:59 -0500

    Add tally build and test

--- generate_test_list.sh
+++ generate_test_list.sh
@@ -13,5 +13,36 @@ DAGMC/tests/dagsolid_unit_tests --gtest_list_tests  | python gen_test_list.py DA
 DAGMC/tests/uwuw_unit_tests --gtest_list_tests | python gen_test_list.py UWUW.
 DAGMC/tests/uwuw_unit_tests --gtest_list_tests | python gen_test_list.py UWUW. >> tasklist.nmi
 
+# Add Tally unit tests
+DAGMC/tests/test_CellTally --gtest_list_tests | python gen_test_list.py CellTally.
+DAGMC/tests/test_CellTally --gtest_list_tests | python gen_test_list.py CellTally. >> tasklist.nmi
+
+DAGMC/tests/test_KDEKernel --gtest_list_tests | python gen_test_list.py KDEKernel.
+DAGMC/tests/test_KDEKernel --gtest_list_tests | python gen_test_list.py KDEKernel. >> tasklist.nmi
+
+DAGMC/tests/test_KDENeighborhood --gtest_list_tests | python gen_test_list.py KDENeighborhood.
+DAGMC/tests/test_KDENeighborhood --gtest_list_tests | python gen_test_list.py KDENeighborhood. >> tasklist.nmi
+
+DAGMC/tests/test_PolynomialKernal --gtest_list_tests | python gen_test_list.py PolynomialKernal.
+DAGMC/tests/test_PolynomialKernal --gtest_list_tests | python gen_test_list.py PolynomialKernal. >> tasklist.nmi
+
+DAGMC/tests/test_Quadrature --gtest_list_tests | python gen_test_list.py Quadrature.
+DAGMC/tests/test_Quadrature --gtest_list_tests | python gen_test_list.py Quadrature. >> tasklist.nmi
+
+DAGMC/tests/test_Tally --gtest_list_tests | python gen_test_list.py Tally.
+DAGMC/tests/test_Tally --gtest_list_tests | python gen_test_list.py Tally. >> tasklist.nmi
+
+DAGMC/tests/test_TallyEvent --gtest_list_tests | python gen_test_list.py TallyEvent.
+DAGMC/tests/test_TallyEvent --gtest_list_tests | python gen_test_list.py TallyEvent. >> tasklist.nmi
+
+DAGMC/tests/test_TallyData --gtest_list_tests | python gen_test_list.py TallyData.
+DAGMC/tests/test_TallyData --gtest_list_tests | python gen_test_list.py TallyData. >> tasklist.nmi
+
+DAGMC/tests/test_CellTally --gtest_list_tests | python gen_test_list.py CellTally.
+DAGMC/tests/test_CellTally --gtest_list_tests | python gen_test_list.py CellTally. >> tasklist.nmi
+
+DAGMC/tests/test_KDEMeshTally --gtest_list_tests | python gen_test_list.py KDEMeshTally.
+DAGMC/tests/test_KDEMeshTally --gtest_list_tests | python gen_test_list.py KDEMeshTally. >> tasklist.nmi
+
 exit $?
 
--- run-test.sh
+++ run-test.sh
@@ -13,5 +13,14 @@ cd $OWD/DAGMC/tests
 ./fludag_unit_tests --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/FLUDAG.//g'`
 ./dagsolid_unit_tests --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/DAGSOLID.//g'`
 ./uwuw_unit_tests --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/UWUW.//g'`
+./test_KDEKernel  --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/KDEKernel.//g'`
+./test_KDENeighborhood  --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/KDENeighborhood.//g'`
+./test_Quadrature  --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/Quadrature.//g'`
+./test_PolynomialKernel  --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/PolynomialKernel.//g'`
+./test_Tally  --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/Tally.//g'`
+./test_CellTally  --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/CellTally.//g'`
+./test_TallyData  --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/TallyData.//g'`
+./test_TallyEvent  --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/TallyEvent.//g'`
+./test_KDEMeshTally  --gtest_filter=`echo ${_NMI_TASKNAME} | sed -e 's/__/\//g' | sed -e 's/KDEMeshTally.//g'`
 
 exit $?
